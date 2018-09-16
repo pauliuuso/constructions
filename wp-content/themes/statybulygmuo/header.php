ea9248b9202966d553f6aa3217446d1482b9ff22
silent-hero <p.jazauskas@gmail.com>
2018-09-16 21:49:31 +0300

    fixed stylesheets

--- wp-content/themes/statybulygmuo/header.php
+++ wp-content/themes/statybulygmuo/header.php
@@ -5,8 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="description" content="<?php bloginfo('description'); ?>">
-        <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/animate.css_3.5.2.css">
-        <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/app.css">
+        <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/style.css">
         <script src="<?php bloginfo('template_url'); ?>/js/core/jquery-3.3.1.min.js"></script>
         <script src="<?php bloginfo('template_url'); ?>/js/core/tether_1.4.0.min.js"></script>
         <script src="<?php bloginfo('template_url'); ?>/js/core/bootstrap.min.js"></script>
